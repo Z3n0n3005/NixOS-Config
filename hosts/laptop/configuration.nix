98ad6083c22e06d9d7a7fff37ecf60d4240dd05d
nixos pc trivy <trivyluong@gmail.com>
2024-08-11 08:53:08 +0700

    Update laptop configuraion

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./main-user.nix
+      inputs.home-manager.nixosModules.default
     ];
 
   # Bootloader.
