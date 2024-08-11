189b9e6efd42ec342b9f1a71412482b3ada145bb
Nix Laptop trivy <trivyluong@gmail.com>
2024-08-11 08:11:10 +0700

    Added laptop host

---
+++ hosts/laptop/main-user.nix
@@ -0,0 +1,54 @@
+{ lib, config, pkgs, ... }:
+
+let
+  cfg = config.main-user;
+in
+{
+  options.main-user = {
+    enable = lib.mkEnableOption "enable user module";
+
+    userName = lib.mkOption {
+      default = "mainuser";
+      description = ''
+        username
+      '';
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    users.users.${cfg.userName} = {
+      isNormalUser = true;
+      description = "TriVy";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        "docker"
+      ];
+      packages = with pkgs; [
+        firefox
+        kate
+        # neovim
+        # htop-vim
+        # (vscode-with-extensions.override {
+        #   vscodeExtensions = with vscode-extensions; [
+        #     bbenoist.nix
+        #     ms-python.python
+        #     ms-vscode-remote.remote-ssh
+        #     ms-toolsai.jupyter
+        #     vscodevim.vim
+        #     jnoortheen.nix-ide
+        #     dracula-theme.theme-dracula
+        #   ];
+        # })
+        # python3
+        # python312Packages.pip
+        # python312Packages.notebook
+        # python312Packages.jupyterlab
+        # python312Packages.ipykernel  
+        # zoom-us
+        # jupyter    
+        # nix-search-cli
+      ];
+    };
+  };
+}
